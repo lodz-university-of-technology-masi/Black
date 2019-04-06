4bd17877bfb4d530f63178d29089b696be1db956
Maksymilian Ciak <195568@edu.p.lodz.pl>
2019-04-06 13:58:09 +0200

    MAS19BLA-14 Przeniesienie plikow.

--- server/scripts/create_database.bat
+++
@@ -1,4 +0,0 @@
-set PGPASSWORD=postgres
-psql -w -U postgres -c "CREATE DATABASE masi;"
-psql -w -U postgres -d masi -f "./init_database.sql"
-pause
--- server/scripts/create_database.sh
+++
@@ -1,3 +0,0 @@
-export PGPASSWORD=postgres
-psql -w -U postgres -c "CREATE DATABASE masi;"
-psql -w -U postgres -d masi -f "./init_database.sql"
