2132b0ab34f272065f9e1b9cabd6ff5bf3382c00
Wiktor Sikora <195703@edu.p.lodz.pl>
2019-04-10 20:29:16 +0200

    MAS19BLA-15 Konfiguracja logowania #time 2h

--- backend/src/main/java/pl/masi/config/SecurityConfiguration.java
+++ backend/src/main/java/pl/masi/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package pl.masi.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.masi.service.UserService;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
@@ -26,11 +29,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        // TODO konfiguracja SpringSecurity
-        httpSecurity
+        httpSecurity.csrf().disable()
                 .authorizeRequests()
-                .anyRequest().permitAll()
-        .and().csrf().disable();
+                .antMatchers(HttpMethod.POST, "/user/signin").permitAll()
+                .antMatchers(HttpMethod.POST, "/user/signout").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/user/signin")
+                    .loginProcessingUrl("/user/signin")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .successHandler((request, response, authentication) -> response.setStatus(HttpServletResponse.SC_NO_CONTENT))
+                    .failureHandler((request, response, exception) -> response.setStatus(HttpServletResponse.SC_FORBIDDEN))
+                .and()
+                    .logout()
+                    .logoutUrl("/user/signout")
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                .and()
+                .httpBasic();
+
     }
 
     @Override
