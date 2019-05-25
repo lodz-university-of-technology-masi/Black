a622081194c2ff41fed4d8c2072084ebfbc8b1ff
Zwierzakkkkk <kamil.stencel1996@gmail.com>
2019-05-25 19:02:16 +0200

    Controller Update

--- backend/src/main/java/pl/masi/controller/EvaluationController.java
+++ backend/src/main/java/pl/masi/controller/EvaluationController.java
@@ -2,11 +2,17 @@ package pl.masi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.masi.controller.base.EntityController;
+import pl.masi.email.EmailService;
 import pl.masi.entity.Evaluation;
 import pl.masi.service.EvaluationService;
 import pl.masi.service.base.EntityService;
+
+import javax.validation.ValidationException;
 
 @Controller
 @RequestMapping(value = "/evaluations")
@@ -15,8 +21,22 @@ public class EvaluationController extends EntityController<Evaluation> {
     @Autowired
     private EvaluationService service;
 
+    @Autowired
+    private EmailService emailService;
+
     @Override
     protected EntityService<Evaluation> getEntityService() {
         return service;
     }
+
+
+    @PostMapping(path = "/{id}/sendemail")
+    void  sendEmail(@PathVariable(name = "id") Long id, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException("Brak oceny");
+        }
+        Evaluation evaluation = service.findById(id).get();
+        emailService.sendEmail(evaluation.getTestAnswer().getUser().getEmail(), evaluation.getContent());
+    }
 }
+
