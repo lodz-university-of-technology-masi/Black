606703756823d5d3eed78c0a38e8a4e22e9edfbb
Zwierzakkkkk <kamil.stencel1996@gmail.com>
2019-05-25 17:42:52 +0200

    Update EmailController.java

--- backend/src/main/java/pl/masi/email/EmailController.java
+++ backend/src/main/java/pl/masi/email/EmailController.java
@@ -1,9 +1,12 @@
 package pl.masi.email;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.masi.entity.Evaluation;
+import pl.masi.entity.User;
 
 import javax.validation.ValidationException;
 
@@ -11,12 +14,20 @@ import javax.validation.ValidationException;
 @RequestMapping("/email")
 public class EmailController {
 
+    @Autowired
+    private EmailService emailService;
+
 
     @PostMapping
     public void sendEmail( BindingResult bindingResult){
         if(bindingResult.hasErrors()){
             throw new ValidationException("Brak oceny");
         }
+        User user=  ;
+        Evaluation evaluation=    ;
+
+        emailService.sendEmail(user.getEmail,evaluation.getContent);
+
 
 
 
